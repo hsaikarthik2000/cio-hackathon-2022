cad7239e895ecafc02b6fbf3c7bca9519c19f41b
hsaikarthik2000 <104208903+hsaikarthik2000@users.noreply.github.com>
2022-10-12 16:01:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import json
+from python3
 
 token = "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"
 headers = {"Authorization" : "token {}".format(token)}
